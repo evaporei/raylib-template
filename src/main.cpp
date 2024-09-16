9ddad9651c124e067c9d0c72343c85401b611649
Eva Pace <eba.pachi@gmail.com>
2024-09-16 19:18:18 -0300

    add thirdparty lib raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include "stdio.h"
+#include "raylib.h"
 
 int main(void) {
-    printf("mario\n");
+#ifndef DEBUG
+    SetTraceLogLevel(LOG_ERROR);
+#endif
+
+    InitWindow(800, 450, "mario");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("mario will jump here.", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
